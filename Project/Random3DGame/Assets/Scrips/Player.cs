87e017c367a1b71669b9f0c47a40191cee03cc5d
Ryan Massia <ryanmassia6@gmail.com>
2020-10-27 11:42:10 -0400

    repawn script functions moved to player script

--- Project/Random3DGame/Assets/Scrips/Player.cs
+++ Project/Random3DGame/Assets/Scrips/Player.cs
@@ -5,39 +5,34 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     GameObject playerModel; // player object
+    public GameObject spawnPoint; // spawn point of player after death 
+    //public GameObject checkPoint; // safe points throughout the level
     public float health; // players health
     public float deaths; // death counter 
-    public GameObject spawnPoint; // spawn point of player after death 
-    public GameObject checkPoint; // safe points throughout the level
-    //public GameObject deathPoint; // place where death occurs 
 
     // Start is called before the first frame update
     void Start()
     {
         health = 100.0f;
-        playerModel = GameObject.Find("Player"); // Looks for player opbject
+        playerModel = GameObject.Find("Player Model"); // Looks for player object
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Needs work PlayerDeath();
+        
     }
 
     void OnTriggerEnter(Collider other)
     {
-        
-        Spawn();
-       
-        
+        if(other.CompareTag("Out of Bounds"))
+        {
+            Spawn();
+        }  
     }
     
-
     void Spawn()
     {
         playerModel.transform.position = spawnPoint.transform.position;
-    }
-
-    //Needs work 
-    
+    }    
 }

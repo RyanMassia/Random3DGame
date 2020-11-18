0e796cfa88532033aa6645329ac07a84a16d19b8
Ryan Massia <ryanmassia6@gmail.com>
2020-11-18 12:10:31 -0500

    Moving Platforms Scripted Adjusted

--- Project/Random3DGame/Assets/Scripts/MovingPlatform.cs
+++ Project/Random3DGame/Assets/Scripts/MovingPlatform.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    public GameObject player;
-
     private void OnTriggerEnter(Collider other)
     {
-        player.transform.parent = transform;
+        if (other.CompareTag("Player")) // Looks for the player tag 
+        {
+            other.gameObject.transform.parent = transform; // sets transform to same as platform so player doesnt fall off 
+        }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider other) 
     {
-        player.transform.parent = null;
+        if (other.CompareTag("Player")) // Looks for the player tag 
+        {
+            other.gameObject.transform.parent = null; // upon leabing the platform transform parent is reset 
+        }
+
     }
 }
